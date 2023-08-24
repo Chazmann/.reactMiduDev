92695204b7a34f23f5b8c36e20b9a6ea134a3397
Leandro Chazarreta <91677149+Chazmann@users.noreply.github.com>
2023-08-24 14:59:18 -0300

    boton comp

---
+++ src/assets/components/Boton.jsx
@@ -0,0 +1,4 @@
+
+export const Boton = ({text})=> { return (
+    <button>{text}</button>
+  )}
--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { Boton } from './assets/components/Boton.jsx'
 const lista = [
   "Confirmar", "Cancelar", "Fiumba"]
 
-const Button = ({text})=> { return (
-  <button>{text}</button>
-)}
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-     <Button text={lista[0]} />
-     <Button text={lista[1]} />
+     <Boton text={lista[0]} />
+     <Boton text={lista[1]} />
   </React.StrictMode>,
 )
