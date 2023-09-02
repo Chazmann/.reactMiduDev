2c379a4a9ad2db8f39530498a2c558152350e29f
Leandro Chazarreta <91677149+Chazmann@users.noreply.github.com>
2023-09-02 15:23:09 -0300

    Creación de componente FollowContainer

---
+++ src/assets/components/FollowContainer.jsx
@@ -0,0 +1,22 @@
+import { Boton } from './Boton.jsx'
+import {CardMiniconuseState} from './CardMiniconuseState.jsx'
+
+export const FollowContainer = () => {
+    const lista = [
+        "Confirmar", "Cancelar", "Fiumba"]
+    return (
+        <div className='fContainer'>
+            <div className="followContainer">
+                <CardMiniconuseState isFollowing userName="Leandro Chazarreta" userNick="Chazmann" />
+                <CardMiniconuseState isFollowing userName="Verónica Moreno Flores" userNick="veromoreno" />
+                <CardMiniconuseState isFollowing={false} userName="Alan Buscaglia " userNick="Gentleman-Programming" />
+            </div>
+            <div className="buttonContainer">
+                <Boton text={lista[1]} />
+                <Boton text={lista[1]} />
+                <Boton text={lista[1]} />
+            </div>
+
+        </div>
+    )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -2,24 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { Boton } from './assets/components/Boton.jsx'
-import { CardMini } from './assets/components/CardMini.jsx'
-import { CardMiniconuseState } from './assets/components/CardMiniconuseState.jsx'
-const lista = [
-  "Confirmar", "Cancelar", "Fiumba"]
+import { FollowContainer } from './assets/components/FollowContainer.jsx'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-
-    <div className="followContainer">
-      <CardMiniconuseState isFollowing userName="Leandro Chazarreta" userNick="Chazmann" />
-      <CardMini isFollowing userName="Verónica Moreno Flores" userNick="veromoreno" />
-      <CardMini isFollowing={false} userName="Alan Buscaglia " userNick="Gentleman-Programming" />
-    </div>
-    <div className="buttonContainer">
-      <Boton text={lista[0]} />
-      <Boton text={lista[2]} />
-    </div>
-
+    <FollowContainer />
   </React.StrictMode>,
 )
