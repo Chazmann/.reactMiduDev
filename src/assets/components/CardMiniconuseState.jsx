d1d2e721a87f8725d4117b1348fcb024df6e0635
Leandro Chazarreta <91677149+Chazmann@users.noreply.github.com>
2023-09-03 17:57:42 -0300

    A quien seguir completado

--- src/assets/components/CardMiniconuseState.jsx
+++ src/assets/components/CardMiniconuseState.jsx
@@ -1,36 +1,35 @@
 import './cardmini.css'
 import { useState } from 'react'
 
-export const CardMiniconuseState = ({userName, userNick}) => {
-   const [isFollowing, setIsFollowing] = useState(false)
+export const CardMiniconuseState = ({ userName, userNick }) => {
+    const [isFollowing, setIsFollowing] = useState(false)
 
     //==> es igual a ==>
 
     // const state = useState(false)
     // const isFollowing = state[0]
     // const setIsFollowing = state[1]
-const handleClick = () =>{
-    setIsFollowing(!isFollowing)
-}
-const butonClass = isFollowing ? 'follow following' : 'follow unfollow'
-   
-   const textBoton = isFollowing ? 'Siguiendo' : 'Seguir'
+    const handleClick = () => {
+        setIsFollowing(!isFollowing)
+    }
+    const butonClass = isFollowing ? 'follow following' : 'follow unfollow'
+
+    const textBoton = isFollowing ? 'Siguiendo' : 'Seguir'
     return (
         <>
-        
             <article className="tw-followCard">
                 <header>
                     <img src={`https://unavatar.io/${userNick}`} alt={userName} />
                     <div>
-                            <strong>{userName}</strong>
+                        <strong>{userName}</strong>
                         <span>{userNick}</span>
                     </div>
                 </header>
                 <aside><button className={butonClass} onClick={handleClick}>
                     <text className='followText'>{textBoton}</text>
                     <text className='unfollowText'>Dejar de seguir</text>
-                    </button></aside>
+                </button></aside>
             </article>
-        </> 
+        </>
     )
 }
